d410b60ea40bd2dec97b28630797641348cba420
Drannnor <chicharo.alexandre@gmail.com>
2017-12-16 18:50:36 +0000

    ficheros .py

---
+++ hobbes_jogo_51.py
@@ -0,0 +1,38 @@
+import jogos_iia.py
+
+class JogoPeoes(jogos_iia.Game) :
+
+    def __init__(game, state, nivel = 0):
+        """O construtor."""
+
+
+        return
+
+
+    def actions(self, state):
+        """Obtencao das jogadas possiveis, dado um estado do jogo."""
+5
+        return
+
+    def result(self,state, move):
+        """Obtencao do estado que se obtem ao executar uma dada jogada num dado estado."""
+
+        return
+
+    def utility(self, state, player):
+        """Calculo da utilidade de um estado na perspectiva de um dado jogador.
+        Devera ter o valor 1, para o caso de vitoria, ou −1, para o caso de derrota."""
+
+        return
+
+    def terminal_test(self, state):
+        """metodo booleano que verifica se um estado dado eh final."""
+
+        return
+
+
+    def display():
+        """Mostra uma representacao de um estado do jogo."""
+
+        return
+

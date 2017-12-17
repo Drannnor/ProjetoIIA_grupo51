62ec9af9394c4cf477772cf642e30d388af010d8
DfogoX <diogocariaferreira@gmail.com>
2017-12-17 18:56:41 +0000

    tentativa de heuristica 1

--- hobbes_jogadores_51.py
+++ hobbes_jogadores_51.py
@@ -1,14 +1,13 @@
 import jogos_iia
-import hobbes_jogo
+import hobbes_jogo_51
 
 # TODO: 
 # Jogadores alfabeta para o jogo hobbes (com funções de avaliação)
 # ver peoes_jogadores para exemplo -- SEM BOTAR
 
 
-def f_aval_hobbes_F1(estado,jogador):
-    #TODO:
-    return 
+def f_aval_hobbes_F1(estado,jogador): #FIXME: quanto mais jogadas tem, mais pontos tem, heuristica 1 ;)
+    return len(hobbes_jogo_51.actions(estado))
 
 def jogador_hobbes_F1(jogo,estado, nivel = 5) : #FIXME:
     return jogos_iia.alphabeta_cutoff_search(estado,jogo,nivel,eval_fn=f_aval_hobbes_F1)

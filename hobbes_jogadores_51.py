e4fa6df9791ef5815c42fda3f312f3f72189254c
Drannnor <chicharo.alexandre@gmail.com>
2017-12-18 11:01:57 +0000

    Drannnor

--- hobbes_jogadores_51.py
+++ hobbes_jogadores_51.py
@@ -1,7 +1,7 @@
 import jogos_iia
 import hobbes_jogo_51
 
-# TODO: 
+# TODO:
 # Jogadores alfabeta para o jogo hobbes (com funções de avaliação)
 # ver peoes_jogadores para exemplo -- SEM BOTAR
 
@@ -11,18 +11,18 @@ def f_aval_hobbes_F1(estado, jogador):
 
 
 # TODO:
-def f_aval_hobbes_F2(estado,jogador):
-    return 
+def f_aval_hobbes_F2(estado, jogador):
+    return
 
 
 # TODO:
-def f_aval_hobbes_F3(estado,jogador):
-    return 
+def f_aval_hobbes_F3(estado, jogador):
+    return
 
 
 # TODO:
-def f_aval_hobbes_F4(estado,jogador):
-    return 
+def f_aval_hobbes_F4(estado, jogador):
+    return
 
-def jogador_hobbes_F1(jogo,estado, nivel = 5) : #FIXME:
-    return jogos_iia.alphabeta_cutoff_search(estado,jogo,nivel,eval_fn=f_aval_hobbes_F1)
+def jogador_hobbes_F1(jogo, estado, nivel=5): #FIXME:
+    return jogos_iia.alphabeta_cutoff_search(estado, jogo, nivel, eval_fn=f_aval_hobbes_F1)
